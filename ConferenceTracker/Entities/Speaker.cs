821477cd6636326415765c6e880dbef8926935b6
Isaac Zinman <izinman@osisoft.com>
2022-05-31 10:28:06 -0700

    First page of work (speakers) done

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,46 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         [Display(Name = "First name")]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
         public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         [Display(Name = "Last name")]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
         public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(maximumLength: 500, MinimumLength = 10)]
         public string Description { get; set; }
+
         [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
+
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var validationResults = new List<ValidationResult>();
+            if (EmailAddress != null && EmailAddress?.EndsWith("TechnologyLiveConference.com"))
+            {
+                validationResults.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+            return validationResults;
+        }
     }
 }
